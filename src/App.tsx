c7c264b4052d679680e2ce8c4970291f13aa49cd
SulaimanAshimzhanov <sulajmanasimzanov@gmail.com>
2025-05-03 23:55:38 +0600

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="">
+
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Header from './header/Header';
+
+export const Components = {
+  Header,
+};
